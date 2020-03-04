402b71ebfb9ff40ce1cb63ef8c76200f113f9dd9
khyatinp18 <khyatinp18@gmail.com>
2020-03-03 19:03:05 -0500

    StarWars API

---
+++ StarwarsAPI2/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StarwarsAPI2.Models;
+
+namespace StarwarsAPI2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://swapi.co/api/");
+
+            var response = await client.GetAsync($"people");
+            var people = await response.Content.ReadAsAsync<PeopleRootObject>();
+
+            return View(people);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StarwarsAPI2/Controllers/StarWarsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarwarsAPI2.Models;
+
+namespace StarwarsAPI2.Controllers
+{
+   
+    public class StarWarsController : Controller
+    {
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetPeopleById(int id)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://swapi.co/api/");
+
+            var response = await client.GetAsync($"people/{id}");
+            var people = await response.Content.ReadAsAsync<Result>();
+
+            return View(people);
+        }
+
+
+        public async Task<IActionResult> GetPlanetById(int id)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://swapi.co/api/");
+
+            var response = await client.GetAsync($"planets/{id}");
+            var planet = await response.Content.ReadAsAsync<Planet>();
+
+            return View(planet);
+        }
+
+        public async Task<IActionResult> GetPeopleList(int id)
+
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://swapi.co/api/");
+
+            var response = await client.GetAsync("people");
+            var people = await response.Content.ReadAsAsync<PeopleRootObject>();
+
+            return View(people);
+        }
+
+
+
+    }
+}
